5e1dbf2dc21cffba459c4c331ee4723eb4cf18f2
Daniel Stoychev <daniel.stoychev@siteground.com>
2026-02-19 22:19:13 +0200

    add header style

--- ToDo App/src/components/Header.jsx
+++ ToDo App/src/components/Header.jsx
@@ -1,14 +1,22 @@
 import styles from "./Header.module.css";
 
+const date = new Date().toDateString();
+
 export default function Header() {
   return (
     <>
-      <div className="header-container">
-        <section className="title">
+      <div className={styles.headerContainer}>
+        <section className={styles.title}>
           <h1>Task Manager</h1>
+          <p>{date}</p>
         </section>
-        <section className="date">
-          <p>{Date()}</p>
+        <section className={styles.date}>
+          <span>
+            completed:<span>test</span>
+          </span>
+          <span>
+            all:<span>test</span>
+          </span>
         </section>
       </div>
     </>
